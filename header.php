dc3ab889d50c60a6607a905659bd319785a6b85d
Rayzeur <louis_langlet@yahoo.fr>
2022-06-22 14:43:03 +0200

    début menu déroulant

--- header.php
+++ header.php
@@ -11,13 +11,36 @@
         <div class="wrapper-menu">
             <div class="logo-menu"><img src="https://institutadios.com/wp-content/uploads/2021/11/Logo-entête.png" alt="Logo" class="img-logo"></div>
             <div class="menu-menu">
-                <div class="categorie-menu"><p>La methode</p></div>
-                <div class="categorie-menu"><p>Addictions </p></div>
+                <div class="categorie-menu"><p>La methode</p>
+                    <ul class="sub-menu">
+                        <li>La methode</li>
+                        <li>Qui somme nous</li>
+                        <li>Avantage et bienfait</li>
+                        <li> livre de référance </li>
+                    </ul>
+                </div>
+                <div class="categorie-menu"><p>Addictions </p>
+                    <ul class="sub-menu">
+                        <li>Tous savoir sur l'acolisme</li>
+                        <li>arreté le tabac</li>
+                        <li>addiction commune </li>
+                        <li>autre addiction</li>
+                        <li>se faire aider </li>
+                        <li>suije vraiment dépendant</li>
+                    </ul>
+                </div>
                 <div class="categorie-menu"><p>Témoignages </p></div>
-                <div class="categorie-menu"><p>Contact </p></div>
+                <div class="categorie-menu"><p>Contact </p>
+                    <ul class="sub-menu">
+                        <li>Contact</li>
+                        <li>Nos insitutue de praticien</li>
+                        <li>Téléchargé notre guide</li>
+                        <li>Ma premiere scéance</li>
+                    </ul>
+                </div>
                 <div class="categorie-menu"><p>0123456789 </p></div>
-                <div class="button-menu"><p>Se Faire aider </p></div>
-                <div class="button-menu"><p>Se former </p></div>
+                <div class="button-menu"><button id="button-1menu">Se Faire aider</button></div>
+                <div class="button-menu"><button id="button-2menu">Se former</button></div>
         
             </div>
         </div>
